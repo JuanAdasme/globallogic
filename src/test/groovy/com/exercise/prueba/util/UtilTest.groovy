5a621d24ee492eed7a7aa2e8963c97727d411067
Juan Adasme <jadasmetoro@gmail.com>
2022-06-30 14:02:36 -0400

    test(utils): add map to user

--- src/test/groovy/com/exercise/prueba/util/UtilTest.groovy
+++ src/test/groovy/com/exercise/prueba/util/UtilTest.groovy
@@ -1,5 +1,6 @@
 package com.exercise.prueba.util
 
+import com.exercise.prueba.model.User
 import spock.lang.Specification
 
 class UtilTest extends Specification {
@@ -7,6 +8,17 @@ class UtilTest extends Specification {
     Utils utils = new Utils()
 
     def "MapUserToUserDTO"() {
+        given:
+            def user = new User()
+            user.setName("Django Freeman")
+            user.setEmail("django@test.cl")
+
+        when:
+            def userDTO = utils.mapUserToUserDTO(user)
+
+        then:
+            user.getName() == userDTO.getName()
+            user.getEmail() == userDTO.getEmail()
     }
 
     def "IsValidPassword"() {
